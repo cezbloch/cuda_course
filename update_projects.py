c161083aacbc25e7438cd6977a2449730c72d08c
Cezary <cez.bloch@gmail.com>
2019-05-09 21:17:33 +0200

    vcxproj modifying scrupt - remove cuda target
    it actually does not work because xml namespaces are removed

--- update_projects.py
+++ update_projects.py
@@ -25,10 +25,25 @@ def find_visual_studio_projects():
 def make_project_cuda_version_independent(project):
     cuda_props = "$(SolutionDir)cuda.props"
     tree = El.parse(project)
-    root = tree
+    root = tree.getroot()
     cuda_props_found = find_attribute(root, cuda_props)
     if not cuda_props_found:
-        pass
+        remove_cuda_target(root)
+        tree.write("c:\out.xml")
+
+
+def remove_cuda_target(root):
+    cuda_targets = "$(VCTargetsPath)\BuildCustomizations\CUDA 10.0.targets"
+    for child in root:
+        if 'Label' in child.attrib:
+            label = child.attrib['Label']
+            if label == 'ExtensionTargets':
+                for import_tag in child:
+                    if 'Project' in import_tag.attrib:
+                        project_attribute = import_tag.attrib['Project']
+                        if project_attribute == cuda_targets:
+                            root.remove(child)
+
 
 
 def find_attribute(root, at):
